6cf0bbe7c2cc80d8a013ad965f13508bb88e7cd0
Fahim Islam <tjfahim1997@gmail.com>
2022-01-08 19:58:49 +0600

    first commit

---
+++ src/components/hello.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Hello extends React.Component {
+    render() {
+        return (
+
+            <
+            h1 >
+            main Component <
+            /h1>
+
+        )
+
+    }
+}
+
+export default Hello;
